a52f6918564025439bec64a07eccc2309b7564a3
jd-13 <jackd8989@gmail.com>
2017-11-19 16:56:55 +0000

    pylint warnings

--- TrainingHelpers.py
+++ TrainingHelpers.py
@@ -32,8 +32,10 @@ class EarlyStoppingHelper:
             assign_ops = {gvar_name: tf.get_default_graph().get_operation_by_name(gvar_name + "/Assign")
                           for gvar_name in gvar_names}
 
-            init_values = {gvar_name: assign_op.inputs[1] for gvar_name, assign_op in assign_ops.items()}
-            feed_dict = {init_values[gvar_name]: self.bestModelParams[gvar_name] for gvar_name in gvar_names}
+            init_values = {gvar_name: assign_op.inputs[1]
+                           for gvar_name, assign_op in assign_ops.items()}
+            feed_dict = {init_values[gvar_name]: self.bestModelParams[gvar_name]
+                         for gvar_name in gvar_names}
             tf.get_default_session().run(assign_ops, feed_dict=feed_dict)
 
             success = True
@@ -57,7 +59,7 @@ class ProgressCalculator:
     Provided a number of iterations, if updateInterval is called on completion of each iteration
     then the time to complete the remaining iterations is calculated.
     """
-    _deltaPerSecond = 0
+    _itersPerSecond = 0
     _progress = 0
     _target = 0
     _startEpoch = 0
@@ -71,7 +73,7 @@ class ProgressCalculator:
 
     def reset(self):
         """Reset the timer and all internal counters"""
-        self._deltaPerSecond = 0
+        self._itersPerSecond = 0
         self._progress = 0
         self._target = 0
         self._startEpoch = 0
@@ -84,20 +86,24 @@ class ProgressCalculator:
         self._progress += newlyCompletedIterations
 
         timeSinceStart = time.time() - self._startEpoch
-        self._deltaPerSecond = self._progress / timeSinceStart
+        self._itersPerSecond = self._progress / timeSinceStart
 
     def getSecondsRemaining(self):
-        """Predict the time to complete the remaining iterations"""
+        """Predict the number of seconds to complete the remaining iterations"""
         try:
-            return (self._target - self._progress) / self._deltaPerSecond
+            return (self._target - self._progress) / self._itersPerSecond
         except ZeroDivisionError:
             return 0
 
     def getTimeStampRemaining(self):
+        """
+        Predict the time to complete the remaining iterations and return a formatted timestamp.
+        """
         timestamp = datetime.timedelta(seconds=self.getSecondsRemaining())
         return str(timestamp)
 
     def timeTaken(self):
+        """Returns a formatted timestamp of the total time since start was called."""
         timeSinceStart = time.time() - self._startEpoch
         timestamp = datetime.timedelta(seconds=timeSinceStart)
         return str(timestamp)
