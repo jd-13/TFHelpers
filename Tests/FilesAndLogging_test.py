cbba9484c6ab782df2460701f4149e65d122cc19
jd-13 <jackd8989@gmail.com>
2017-12-17 19:40:41 +0000

    Add basic unit tests

---
+++ Tests/FilesAndLogging_test.py
@@ -0,0 +1,35 @@
+"""
+Tests for functionality in the FilesAndLogging module.
+"""
+
+from datetime import datetime
+import os
+
+from TFHelpers import FileManager
+
+class Test_FileManagerTests:
+    """
+    Tests for the FileManager class.
+    """
+    def test_WithoutRestore(self):
+        """
+        Tests if the correct model paths are generated if no model to restore from is provided.
+        """
+
+        manager = FileManager("TestRegressor")
+        TIMESTAMP = datetime.utcnow().strftime("%Y%m%d-%H%M")
+
+        assert manager.getModelDir() == os.getcwd() + "/models/TestRegressor/" +  TIMESTAMP
+        assert manager.getModelDirAndPrefix() == os.getcwd() + "/models/TestRegressor/" + TIMESTAMP + "/model"
+
+    def test_WithRestore(self):
+        """
+        Tests if the correct model paths are generated if a model to restore from is provided.
+        """
+
+        RESTORE_FROM = "20171225-1200"
+        manager = FileManager("TestRegressor", RESTORE_FROM)
+
+        assert manager.getModelDir() == os.getcwd() + "/models/TestRegressor/" +  RESTORE_FROM
+        assert manager.getModelDirAndPrefix() == os.getcwd() + "/models/TestRegressor/" + RESTORE_FROM + "/model"
+    
---
+++ Tests/TrainingHelpers_test.py
@@ -0,0 +1,31 @@
+"""
+Tests for functionality in the TrainingHelpers module.
+"""
+
+import pytest
+
+from TFHelpers import ProgressCalculator
+
+class Test_ProgressCalculator:
+    """
+    Tests for the ProgressCalculator class.
+    """
+    def test_InvalidIterations(self):
+        """
+        ProgressCalculator should raise an exception if initialised with an invalid number of
+        iterations.
+        """
+        with pytest.raises(ValueError):
+            ProgressCalculator(-10)
+
+        with pytest.raises(ValueError):
+            ProgressCalculator(0)
+
+    def test_TimeTakenBeforeStart(self):
+        """
+        ProgressCalculator should raise an exception if timeTaken is called before the timer has
+        been started.
+        """
+        progress = ProgressCalculator(10)
+        with pytest.raises(RuntimeError):
+            progress.timeTaken()
